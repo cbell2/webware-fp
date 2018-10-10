41aa11cbdddebdb465e477f3921a1234a0a96e83
Jason <jpking@wpi.edu>
2018-10-10 18:24:43 -0400

    Owner can now decline a users request to join

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -73,44 +73,75 @@ router.post('/requestEvent', function(req, res, next) {
 router.post('/acceptRequest', function(req, res, next) {
     user.findOne({
             _id: req.body.userId
+    }).populate({
+        path: 'eventsApplied',
+        model: 'events'
+    }).populate({
+        path: 'eventsAttending',
+        model: 'events'
+    }).then((someUser) => {
+        events.findOne({
+            _id: req.body.eventId
         }).populate({
-            path: 'eventsApplied',
-            model: 'events'
+            path: 'requested',
+            model: 'user'
         }).populate({
-            path: 'eventsAttending',
-            model: 'events'
-        }).then((someUser) => {
-            events.findOne({
-                _id: req.body.eventId
-            }).populate({
-                path: 'requested',
-                model: 'user'
-            }).populate({
-                path: 'attending',
-                model: 'user'
-            }).then((someEvent)=>{
-                // Remove from requested, add to attending
-                someEvent.attending.push(someUser);
-                for (var i = 0; i < someEvent.requested.length; i++) {
-                    if (someEvent.requested[i]._id.toString() == someUser._id.toString()) {
-                        console.log('YEET');
-                        someEvent.requested.splice(i, 1);
-                    }
+            path: 'attending',
+            model: 'user'
+        }).then((someEvent)=>{
+            // Remove from requested, add to attending
+            someEvent.attending.push(someUser);
+            for (var i = 0; i < someEvent.requested.length; i++) {
+                if (someEvent.requested[i]._id.toString() == someUser._id.toString()) {
+                    someEvent.requested.splice(i, 1);
                 }
-                someEvent.save();
+            }
+            someEvent.save();
 
-                // Remove from applied, add to attending
-                someUser.eventsAttending.push(someEvent);
-                for (var i = 0; i < someUser.eventsApplied.length; i++) {
-                    if (someUser.eventsApplied[i]._id.toString() == someEvent._id.toString()) {
-                        console.log('YEET');
-                        someUser.eventsApplied.splice(i, 1);
-                    }
+            // Remove from applied, add to attending
+            someUser.eventsAttending.push(someEvent);
+            for (var i = 0; i < someUser.eventsApplied.length; i++) {
+                if (someUser.eventsApplied[i]._id.toString() == someEvent._id.toString()) {
+                    someUser.eventsApplied.splice(i, 1);
                 }
-                someUser.save();
-            });
-            res.end();
+            }
+            someUser.save();
         });
+        res.end();
+    });
+});
+
+router.delete('/declineRequest', function(req, res, next) {
+    user.findOne({
+            _id: req.body.userId
+    }).populate({
+        path: 'eventsApplied',
+        model: 'events'
+    }).then((someUser) => {
+        events.findOne({
+            _id: req.body.eventId
+        }).populate({
+            path: 'requested',
+            model: 'user'
+        }).then((someEvent)=>{
+            // Remove from requested
+            for (var i = 0; i < someEvent.requested.length; i++) {
+                if (someEvent.requested[i]._id.toString() == someUser._id.toString()) {
+                    someEvent.requested.splice(i, 1);
+                }
+            }
+            someEvent.save();
+
+            // Remove from applied
+            for (var i = 0; i < someUser.eventsApplied.length; i++) {
+                if (someUser.eventsApplied[i]._id.toString() == someEvent._id.toString()) {
+                    someUser.eventsApplied.splice(i, 1);
+                }
+            }
+            someUser.save();
+        });
+        res.end();
+    });
 });
 
 module.exports = router;
