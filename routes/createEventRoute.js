623f640560c2f3ba8ee22ba9b9b66bcfb753b27f
kmgumienny <kmgumienny@gmail.com>
2018-10-09 23:51:46 -0400

    Adding images to events

--- routes/createEventRoute.js
+++ routes/createEventRoute.js
@@ -4,10 +4,24 @@ var mongoose = require('mongoose');
 var events = require("mongoose").model('events');
 var user = require("mongoose").model('user');
 var mongSetup = require('../mongooseSetup');
+var multer = require('multer');
 
 
 var mongoDB = mongSetup.db;
 mongoDB.Promise = global.Promise;
+
+const multerConfig = {
+    storage: multer.diskStorage({
+        destination: function(req, file, next){
+            next(null, './public/uploads')
+        },
+        filename: function(req, file, next){
+            console.log(file);
+            const ext = file.mimetype.split('/')[1];
+            next(null, file.fieldname + '-' + Date.now() + '.' + ext);
+        }
+    })
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,7 +32,7 @@ router.get('/', function(req, res, next) {
             });
 });
 
-router.post('/', function(req, res, next){
+router.post('/', multer(multerConfig).single('photo'), function(req, res, next){
     if (req.body.bookname &&
         req.body.time &&
         req.body.desc &&
@@ -29,19 +43,29 @@ router.post('/', function(req, res, next){
             message: "SUCCESS"
         });
         console.log('got everything');
-        console.log(req.session.userId);
+
         //TODO create an event
         user.findOne({
             _id: req.session.userId,
         }).populate({
             path: 'eventsOwned',
             model: 'events'}).then((someUser) => {
-            console.log(JSON.stringify(someUser));
+            //console.log(JSON.stringify(someUser));
+            var photoName;
+            //console.log(req.file.filename)
+            if(!req.file)
+                photoName = "../uploads/default.jpeg";
+            else
+                photoName = "../uploads/" + req.file.filename;
+
+            console.log("the photo name is " + photoName);
+
             var newEvent = new events({
                 name: req.body.bookname,
                 date: req.body.time,
                 description: req.body.desc,
                 maxAttendance: req.body.capacity,
+                image: photoName
             });
             newEvent.save().then(()=>{
                 someUser.eventsOwned.push(newEvent);
